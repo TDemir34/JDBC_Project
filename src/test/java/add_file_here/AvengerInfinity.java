67d3e78100b64f702bafacc2a5ff843a6f247351
TDemir34 <72080030+TDemir34@users.noreply.github.com>
2020-10-07 22:30:18 +0200

    Ravenger funs are here....

---
+++ src/test/java/add_file_here/AvengerInfinity.java
@@ -0,0 +1,11 @@
+package add_file_here;
+
+public class AvengerInfinity {
+
+    public static void main(String[] args) {
+
+        System.out.println("Live is good");
+        System.out.println("London");
+        System.out.println("Stockholm");
+    }
+}
